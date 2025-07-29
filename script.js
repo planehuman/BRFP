04031029a3867c0be8cdc6d3fd75ba52672acaf9
YHMspotter <kanesworld2022@outlook.com>
2025-07-29 18:08:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -501,11 +501,12 @@ async function handleGenerate() {
   }
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/submit_flightplan", {
+    const response = await fetch("https://brfpb.onrender.com/submit_flightplan", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(bfpData)
     });
+
 
     const data = await response.json();
 
